f534f784a7d12ffd317a72095603a0f6476d9660
daragmas <daragmas@gmail.com>
2022-06-28 11:22:41 -0400

    Created pokemonTeam Object, added names to images.

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-const pokemon = ['bulbasaur', 'charmander', 'squirtle', 'snorlax']
-const pokemonIDs = ['001', '004', '007','143']
+// const pokemon = ['bulbasaur', 'charmander', 'squirtle', 'snorlax']
+// const pokemonIDs = ['001', '004', '007','143']
+const pokemonTeam = [
+    {name:'Bulbasaur', id:'001'}, 
+    {name:'Charmander', id:'004'}, 
+    {name:'Squirtle', id:'007'},
+    {name:'Snorlax', id:'143'}]
+
 const containerDiv = document.querySelector('#container')
 
 //loop over all ids, 
@@ -7,15 +13,18 @@ const containerDiv = document.querySelector('#container')
 //set html element values, 
 //append to DOM
 
-pokemonIDs.map((id) => {
-    let imageURL = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png`
-    console.log(imageURL)
+pokemonTeam.map((element,index) => {
+    let imageURL = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${element.id}.png`
+    // console.log(pokemon[index])
     
     let div = document.createElement('div')
     div.setAttribute('class', 'pokemon-card')
 
+    let h3 = document.createElement('h3')
+    h3.innerText = element.name
+
     let img = document.createElement('img')
     img.src = imageURL
-    div.append(img)
+    div.append(img, h3)
     containerDiv.append(div)
 })
