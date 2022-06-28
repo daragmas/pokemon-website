72168f64b4168d3d240b4c34755f14bc6b4c8124
daragmas <daragmas@gmail.com>
2022-06-28 16:24:46 -0400

    moved number check

--- main.js
+++ main.js
@@ -9,17 +9,39 @@ const newBtn = document.querySelector('#new-pokemon-btn')
 const rosterDiv = document.querySelector('#roster')
 const userRoster = []
 
-newBtn.addEventListener('click', () => {
+newBtn.addEventListener('click', async () => {
     let num = prompt('Enter National Pokedex Number')
-    if (num<10){num = `00${num}`}
-    else if(num<100){num = `0${num}`}
-    console.log(num)
+    let apiURL = `https://pokeapi.co/api/v2/pokemon/${num}`
+    let req = await fetch(apiURL)
+    let res = await req.json()
+    let name = res.species.name
+    let h3 = document.createElement('h3')
+    h3.innerText = name
+
+    if (num.length < 3) {
+        if (num < 10) { num = `00${num}` }
+        else if (num < 100) { num = `0${num}` }
+    }
+
     let imageURL = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${num}.png`
+
     let img = document.createElement('img')
     img.setAttribute('src', imageURL)
     img.setAttribute('class', 'roster-img')
     let position = document.querySelector(`#pokemon-${userRoster.length+1}`)
-    position.append(img)
+    
+    let audioURL = `https://play.pokemonshowdown.com/audio/cries/${name}.mp3`
+    let audio = document.createElement('audio')
+    let source = document.createElement('source')
+    source.setAttribute('src', audioURL)
+    source.setAttribute('type', 'audio/mpeg')
+    audio.append(source)
+
+    position.addEventListener('click', () => {
+        audio.play()
+    })
+
+    position.append(audio, img, h3)
     userRoster.push(num)
 })
 
