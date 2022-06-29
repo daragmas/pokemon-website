a99303f7cec5c32b4947166ab2cdeec0d004b733
daragmas <daragmas@gmail.com>
2022-06-29 13:34:24 -0400

    added removal confirmation

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 /*
-Allow the user to remove a pokemon from their roster.
-When the user tries to remove a pokemon, make them confirm that they want to remove it before it removes that pokemon.
 In the Pokemon roster section, allow the user to nickname Pokemon in their roster. The user interface (webpage) should display the nickname instead of the Pokemon name if the Pokemon has a nickname.
 */
 
@@ -56,14 +54,16 @@ newBtn.addEventListener('click', async () => {
         userRoster.push(num)
 
         removeBtn.addEventListener('click', () => {
-            let rosterIndex = userRoster.indexOf(num)
-            userRoster.splice(rosterIndex)
+            if (confirm(`Are you sure you want to remove ${name}?`))
+            {
+                let rosterIndex = userRoster.indexOf(num)
+                userRoster.splice(rosterIndex)
 
-            position.removeChild(img)
-            position.removeChild(h3)
-            position.removeChild(audio)
-            position.removeChild(removeBtn)
-        })
+                position.removeChild(img)
+                position.removeChild(h3)
+                position.removeChild(audio)
+                position.removeChild(removeBtn)
+        }})
 
     }
     else{
