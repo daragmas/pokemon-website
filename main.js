6a5bbb6ed0a4582c1e9593b12e28732a20ec7fb5
daragmas <daragmas@gmail.com>
2022-07-01 10:53:27 -0400

    completed ability to fetch data by name

--- main.js
+++ main.js
@@ -1,106 +1,122 @@
-const pokemonTeam = [
-    {name:'Bulbasaur', id:'001'}, 
-    {name:'Charmander', id:'004'}, 
-    {name:'Squirtle', id:'007'},
-    {name:'Snorlax', id:'143'}]
+// const pokemonTeam = [
+//     { name: 'Bulbasaur', id: '001' },
+//     { name: 'Charmander', id: '004' },
+//     { name: 'Squirtle', id: '007' },
+//     { name: 'Snorlax', id: '143' }]
 
 const containerDiv = document.querySelector('#container')
 const newBtn = document.querySelector('#new-pokemon-btn')
 const rosterDiv = document.querySelector('#roster')
 const userRoster = []
 
-const whosThatPokemon= async (pokemon,infoNeeded) => {
+const whosThatPokemon = async (pokemon, infoNeeded) => {
     let apiURL = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
     let req = await fetch(apiURL)
+    // console.log(req)
+    if (req.status == 404){
+        return
+    }
+    else{
     let res = await (req.json())
-    //console.log(res)
     let dataReturn = res[`${infoNeeded}`]
-    //console.log(typeof dataReturn, dataReturn, ' dataReturn')
     return dataReturn
+    }
 }
 
 
 newBtn.addEventListener('click', async () => {
-    if(userRoster.length <6 || userRoster.includes('')){
+    if (userRoster.length < 6 || userRoster.includes('')) {
         let userInput = prompt('Enter National Pokedex Number or Pokemon Name') // get user input\
         let data
-        if (!(parseInt(userInput))){ //if user entered a word, get the pokemon's pokedex number
-            let pokemonName = userInput.toLowerCase() 
-            data = await whosThatPokemon(pokemonName, 'id')
-                console.log(data)
-            }
-        else if(parseInt(userInput)){ //if the user entered a number, get the pokemon's name
+        let num
+        let name
+        if (!(parseInt(userInput))) { //if user entered a word, get the pokemon's pokedex number
+            name = userInput.toLowerCase()
+            data = await whosThatPokemon(name, 'id')
+            num = data
+        }
+        else if (parseInt(userInput)) { //if the user entered a number, get the pokemon's name
             num = parseInt(userInput)
-            data = await whosThatPokemon(num,'name')
-            if(data){
-                let h3 = document.createElement('h3')
-                h3.innerText = data
-                h3.className = 'pokemon-name'
+            data = await whosThatPokemon(num, 'name')
+            name = data
+            console.log(name)
+        }
+        if (data) {
+            let h3 = document.createElement('h3')
+            h3.innerText = name
+            h3.className = 'pokemon-name'
 
-                if (`${num}`.length < 3) {
-                    if (num < 10) { num = `00${num}` }
-                    else if (num < 100) { num = `0${num}` }
+            if (`${num}`.length < 3) {
+                if (num < 10) { num = `00${num}` }
+                else if (num < 100) { num = `0${num}` }
+            }
+
+            let imageURL = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${num}.png`
+
+            let img = document.createElement('img')
+            img.setAttribute('src', imageURL)
+            img.setAttribute('class', 'roster-img')
+
+            let position
+            if (userRoster.length < 6) { position = document.querySelector(`#pokemon-${userRoster.length + 1}`) }
+            else { position = document.querySelector(`#pokemon-${userRoster.indexOf('') + 1}`) }
+            // console.log(position)
+            if(name.includes('-')){
+                name = name.split('-')
+                name=`${name[0]}${name[1]}`
+            }
+            let audioURL = `https://play.pokemonshowdown.com/audio/cries/${name}.mp3`
+            let audio = document.createElement('audio')
+            let source = document.createElement('source')
+            source.setAttribute('src', audioURL)
+            source.setAttribute('type', 'audio/mpeg')
+            audio.append(source)
+
+            function cry() {
+                audio.play()
+            }
+
+
+            position.addEventListener('click', cry)
+
+            let removeBtn = document.createElement('button')
+            removeBtn.innerHTML = 'X'
+            removeBtn.type = 'submit'
+            removeBtn.className = 'remove-pokemon-btn'
+
+            position.append(audio, img, h3, removeBtn)
+
+            if (userRoster.length < 6) { userRoster.push(num) }
+            else { userRoster[userRoster.indexOf('')] = num }
+
+            removeBtn.addEventListener('click', () => {
+                if (confirm(`Are you sure you want to remove ${h3.innerText}?`)) {
+                    let rosterIndex = `${position.id}`
+                    rosterIndex = rosterIndex.split('-')[1] - 1
+                    // console.log(rosterIndex)
+                    userRoster[rosterIndex] = ''
+
+                    position.removeChild(img)
+                    position.removeChild(h3)
+                    position.removeChild(audio)
+                    position.removeEventListener('click', cry)
+                    position.removeChild(removeBtn)
                 }
+            })
 
-                let imageURL = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${num}.png`
-
-                let img = document.createElement('img')
-                img.setAttribute('src', imageURL)
-                img.setAttribute('class', 'roster-img')
-
-                let position
-                if (userRoster.length < 6){position = document.querySelector(`#pokemon-${userRoster.length + 1}`)}
-                else {position = document.querySelector(`#pokemon-${userRoster.indexOf('')+1}`)}
-                // console.log(position)
-                let audioURL = `https://play.pokemonshowdown.com/audio/cries/${data}.mp3`
-                let audio = document.createElement('audio')
-                let source = document.createElement('source')
-                source.setAttribute('src', audioURL)
-                source.setAttribute('type', 'audio/mpeg')
-                audio.append(source)
-
-                function cry() {
-                    audio.play()
-                }
-                
-
-                position.addEventListener('click', cry)
-                
-                let removeBtn = document.createElement('button')
-                removeBtn.innerHTML = 'X'
-                removeBtn.type = 'submit'
-                removeBtn.className = 'remove-pokemon-btn'
-                
-                position.append(audio, img, h3, removeBtn)
-
-                if (userRoster.length<6){userRoster.push(num)}
-                else{userRoster[userRoster.indexOf('')] = num}
-
-                removeBtn.addEventListener('click', () => {
-                    if (confirm(`Are you sure you want to remove ${h3.innerText}?`))
-                    {
-                        let rosterIndex = `${position.id}`
-                        rosterIndex = rosterIndex.split('-')[1]-1
-                        // console.log(rosterIndex)
-                        userRoster[rosterIndex] = ''
-
-                        position.removeChild(img)
-                        position.removeChild(h3)
-                        position.removeChild(audio)
-                        position.removeEventListener('click', cry)
-                        position.removeChild(removeBtn)
-                }})
-
-                h3.addEventListener('click', ()=>{
-                    let currentName = h3.innerText
-                    h3.innerText = prompt("Enter nickname ", `${h3.innerText}`)
-                    if (h3.innerText == ''){h3.innerText = currentName}
-                })}
+            h3.addEventListener('click', () => {
+                let currentName = h3.innerText
+                h3.innerText = prompt("Enter nickname ", `${h3.innerText}`)
+                if (h3.innerText == '') { h3.innerText = currentName }
+            })
+        }
+        else{
+            alert('Invalid input')
         }
     }
-    else{
+    else {
         alert('Your team is full! Transfer a pokemon to the PC to add a new one.')
-    } 
+    }
 })
 
 /*
